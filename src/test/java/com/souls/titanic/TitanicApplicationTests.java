3ad3cb8b9702e0886f5e30711a0fc43e59dad2dd
Mukusha <91588607+Mukusha@users.noreply.github.com>
2024-07-18 20:57:42 +0300

    Add files via upload

---
+++ src/test/java/com/souls/titanic/TitanicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.souls.titanic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TitanicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
